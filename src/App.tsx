960a20faa73f652afb87efa212eadd33bb15f029
Marquis <whitley.marquis1@yahoo.com>
2022-11-12 20:07:37 -0600

    Added Rocket Animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { useState } from 'react'
+import { Dispatch, SetStateAction, useState } from 'react'
 import reactLogo from './assets/react.svg'
+import phoebe from './assets/phoebe.svg'
+// import RocketShip from './assets/rocketship';
+import RocketAnimation from './assets/rocketship'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [count, setCount]:[number, Dispatch<SetStateAction<number>>] = useState(0)
+  const name: string = "Brange";
 
   return (
     <div className="App">
@@ -14,11 +18,12 @@ function App() {
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <img src={phoebe} className="logo react" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + {name}</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount((count) => ++count)}>
+          count is {count}, {name}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
@@ -27,6 +32,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <RocketAnimation />
     </div>
   )
 }
